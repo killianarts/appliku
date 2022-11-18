6f0bf414ba2af24349b400fe230ee9dd4f6aa646
Micah Killian <micah@killianarts.online>
2022-11-18 23:11:27 +0900

    Updated urls.py

--- appliku/urls.py
+++ appliku/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from core import views as core_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    include('core.urls')
+    path('', core_views.index, name='index')
 ]
