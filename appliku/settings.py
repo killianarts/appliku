2143c1b70f1788dbafb203e45e7620d9fc58f22b
Micah Killian <micah@killianarts.online>
2022-11-19 09:24:29 +0900

    Added db

--- appliku/settings.py
+++ appliku/settings.py
@@ -81,13 +81,8 @@ WSGI_APPLICATION = 'appliku.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
